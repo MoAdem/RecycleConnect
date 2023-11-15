537946ee4fd6d295b5bbf8971c074c251c445695
Mohamed Adem  Torkhani <115900442+MoAdem@users.noreply.github.com>
2023-11-15 18:59:11 +0100

    fixing_Duplicate_key_NewsSraping

--- controller/News.js
+++ controller/News.js
@@ -6,13 +6,10 @@ import News from '../model/News.js';
 const newsController = {
 scrapeNewsData : async (url, source, selectors) => {
     try {
-      // Make an HTTP request to the target website
       const response = await axios.get(url);
   
-      // Load the HTML content into Cheerio
       const $ = cheerio.load(response.data);
   
-      // Extract relevant information using custom selectors
       const newsData = $(selectors.articleSelector).slice(0, 3) .map((index, element) => {
         const title = $(element).find(selectors.title).text();
         const newsPhoto = $(element).find(selectors.newsPhoto).attr('src');
@@ -26,7 +23,7 @@ scrapeNewsData : async (url, source, selectors) => {
               source,
             };
           } else {
-            console.warn(`Skipping news article with missing or empty title for source ${source}`);
+            //console.warn(`Skipping news article with missing or empty title for source ${source}`);
             return null;
           }
         }).get().filter(Boolean);
@@ -38,10 +35,9 @@ scrapeNewsData : async (url, source, selectors) => {
     }
   },
   
-  // Controller function to handle news scraping and saving
+
   scrapeAndSaveNews : async (req, res) => {
     try {
-      // Define the websites to scrape with custom selectors
       const websites = [
         {
             url: 'https://www.oxfam.org/en',                           
@@ -65,32 +61,32 @@ scrapeNewsData : async (url, source, selectors) => {
           },
 
 
-        // Add more websites as needed
       ];
   
-      // Use Promise.all to execute scraping functions concurrently
+
       const scrapedNews = await Promise.all(websites.map(site => newsController.scrapeNewsData(site.url, site.source, site.selectors)));
   
-      // Flatten the array of arrays into a single array
       const allNews = [].concat(...scrapedNews);
   
-      // Save scraped news to the database
-      await News.insertMany(allNews);
+      try {
+        await News.insertMany(allNews, { ordered: false }); // Use { ordered: false } to ignore duplicate key errors
+      } catch (error) {
+        if (error.code === 11000) {
+          console.warn(`Duplicate key error: ${error.message}`);
+          // Log or handle the duplicate key error if needed
+        } else {
+          console.error(error);
+          res.status(500).json({ message: 'Internal server error', error: error.message });
+          return; // Stop the process if there's another type of error
+        }
+      }
   
       res.status(200).json({ message: 'News scraping successful', news: allNews });
     } catch (error) {
-        if (error.code === 11000) {
-          // Duplicate key error (E11000), handle it gracefully
-          console.warn(`Duplicate key error: ${error.message}`);
-          res.status(200).json({ message: 'No recent news available' });
-        } else {
-          // Other errors
-          console.error(error);
-          res.status(500).json({ message: 'Internal server error', error: error.message });
-        }
-      }
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
   },
-    // Retrieve all news
     all_news : async (req, res) => {
       try {
         const news = await News.find();
